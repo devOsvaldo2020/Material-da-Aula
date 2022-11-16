d97f2dd9132ae93cf1514bd1d461532dc66371d4
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-11-16 20:20:29 -0300

    script funcao lógica,  css areas.incorrect

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+let areas = {
+    a: null,
+    b: null,
+    c: null
+}
 document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -42,13 +46,44 @@ function drop(e) {
     if (verificacao) {
         let dragItem = document.querySelector('.item.dragging');
         e.currentTarget.appendChild(dragItem);
+        updateAreas();
     };
 };
 function dropNeutral(e) {
     e.currentTarget.classList.remove('hover');
     const dragItem = document.querySelector('.item.dragging');
     e.currentTarget.appendChild(dragItem);
+    updateAreas();
 };
+
+// função lógica
+function updateAreas() {
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+        const verificacao = area.querySelector('.item') !== null;
+        if (verificacao) {
+            areas[name] = area.querySelector('.item').innerHTML;
+        } else {
+            areas[name] = null;
+        }
+    });
+
+    const posicaoErrada = areas.a !== null && areas.b !== null && areas.c !== null;
+    if (posicaoErrada) {
+        document.querySelector('.areas').classList.add('incorrect');
+    } else {
+        document.querySelector('.areas').classList.remove('incorrect');
+    }
+
+    const verificacao = areas.a === '1' && areas.b === '2' && areas.c === '3';
+    if (verificacao) {
+        document.querySelector('.areas').classList.remove('incorrect');
+        document.querySelector('.areas').classList.add('correct');
+    } else {
+        document.querySelector('.areas').classList.remove('correct');
+    }
+
+}
 
 
 
